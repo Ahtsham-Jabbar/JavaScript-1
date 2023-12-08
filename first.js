886285893a398862fc7c28fdc2a4b498b5cb8554
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-12-08 12:45:49 +0500

    add

---
+++ first.js
@@ -0,0 +1,23 @@
+// Practise 1 
+
+const product = {
+    title: "Ball pen",
+    rating: 4,
+    offer: 5,
+    price: 270,
+};
+
+// console.log(product);
+
+
+// Practise 2
+
+const instagram = {
+    username: "@ahtshamjatt03",
+    posts: 125,
+    following: 250,
+    followers: 150,
+    isfollow: true,
+};
+
+console.log(typeof instagram["followers"]);
